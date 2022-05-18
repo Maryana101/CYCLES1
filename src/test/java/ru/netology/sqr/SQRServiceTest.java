375d439ece753db7fe3abfd7f4e5ce77468ceb3c
Марьяна Варфоломеева <mihralievam@icloud.com>
2022-05-18 23:38:06 +0500

    method sqrCount modified

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.netology.sqr.*;
+package ru.netology.sqr;
+
 
 public class SQRServiceTest {
     @Test
     public void shouldCountSqrtForMiddleDiapason () {
         SQRService sqrService = new SQRService();
-        int actual = sqrService.SqrCount(200, 300);
+        int actual = sqrService.sqrCount(200, 300);
         int expected = 3;
 
         Assertions.assertEquals(expected,actual);
@@ -14,7 +15,7 @@ public class SQRServiceTest {
     @Test
    public void shouldCountSqrtForSmallDiapason (){
         SQRService sqrService=new SQRService();
-        int actual=sqrService.SqrCount(0,1);
+        int actual=sqrService.sqrCount(0,1);
         int expected =2;
 
         Assertions.assertEquals(expected,actual);
@@ -22,7 +23,7 @@ public class SQRServiceTest {
     @Test
    public void shouldCountSqrtForBigDiapason (){
         SQRService sqrService=new SQRService();
-        int actual=sqrService.SqrCount(0,10000000);
+        int actual=sqrService.sqrCount(0,10000000);
         int expected=3163;
 
         Assertions.assertEquals(expected,actual);
@@ -30,7 +31,7 @@ public class SQRServiceTest {
     @Test
    public void negativeTest(){
         SQRService sqrService=new SQRService();
-        int actual=sqrService.SqrCount(-100,100);
+        int actual=sqrService.sqrCount(-100,100);
         int expected=11;
 
         Assertions.assertEquals(expected,actual);
